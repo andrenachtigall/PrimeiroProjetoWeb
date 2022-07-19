c38600ecfd9b9f530d2af9bfe91fb24d62177002
andrenachtigall <an.nachtigall@inf.ufpel.edu.br>
2022-07-19 16:31:13 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('.slider-principal').slick({
+  dots: true,
+  infinite: true,
+  speed: 3000,
+  slidesToShow: 1,
+  autoplay: true
+})
+/*
+$('.slider-principal').slick({
+  dots: true,
+  infinite: true,
+  slidesToShow: 3,
+})
+*/
